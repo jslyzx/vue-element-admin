9bbf7e00911ae2248820fc036fa1defcdf0be1ea
jslyzx <821203886@qq.com>
2023-04-11 17:01:44 +0800

    图片

--- src/views/shop/distributed/index.vue
+++ src/views/shop/distributed/index.vue
@@ -16,6 +16,8 @@
   </div>
 </template>
 <script>
+import { grid } from '@/api/shop'
+import _ from 'lodash'
 let BMap = {}
 let map = {}
 export default {
@@ -23,16 +25,36 @@ export default {
   data() {
     return {
       type: '总数',
-      map: {}
+      map: {},
+      shopArr: []
     }
   },
   created() {
     const that = this
-    setTimeout(() => {
-      that.initMap()
-    }, 2000)
+    // setTimeout(() => {
+    //   that.initMap()
+    // }, 2000)
+    this.getShopList()
   },
   methods: {
+    async getShopList() {
+      const res = await grid({
+        page: 1,
+        pageSize: 100
+      })
+      let arr = res.data.data
+      arr = _.filter(arr, function(v){return !!v.axis})
+      let tmp = []
+      this.shopArr = _.map(arr, function(v){
+        tmp = v.axis.split(',')
+        return {
+          name: v.name,
+          longitude: tmp[0],
+          latitude: tmp[1],
+          isDtp: v.isDtp === 1 ? true : false
+        }
+      })
+    },
     initMap() {
       const that = this
       BMap = window.BMap
@@ -77,184 +99,8 @@ export default {
     },
     initMarks() {
       var markers1 = []; var markers2 = []
-      var pointList = [
-        {
-          'name': '南京德众堂大药房有限公司',
-          'longitude': 118.7642593383789,
-          'latitude': 32.043853759765628,
-          'type': 2
-        },
-        {
-          'name': '苏州德轩堂药房有限公司',
-          'longitude': 120.54890441894531,
-          'latitude': 31.4341983795166,
-          'type': 2
-        },
-        {
-          'name': '徐州延顺堂药房有限公司',
-          'longitude': 117.17539978027344,
-          'latitude': 34.26227569580078,
-          'type': 2
-        },
-        {
-          'name': '淮安德轩堂药房有限公司',
-          'longitude': 119.01316833496094,
-          'latitude': 33.62675094604492,
-          'type': 2
-        },
-        {
-          'name': '常州德轩堂药房有限公司',
-          'longitude': 119.959716796875,
-          'latitude': 31.78243637084961,
-          'type': 2
-        },
-        {
-          'name': '南京延顺堂大药房有限公司',
-          'longitude': 118.78749084472656,
-          'latitude': 32.064022064208987,
-          'type': 2
-        },
-        {
-          'name': '无锡德轩堂药房有限公司',
-          'longitude': 120.31443786621094,
-          'latitude': 31.547887802124025,
-          'type': 2
-        },
-        {
-          'name': '扬州德轩堂药房有限公司',
-          'longitude': 119.4072265625,
-          'latitude': 32.3927001953125,
-          'type': 2
-        },
-        {
-          'name': '盐城德轩堂药房有限公司',
-          'longitude': 120.1700668334961,
-          'latitude': 33.38267517089844,
-          'type': 1
-        },
-        {
-          'name': '南通德轩堂药房有限公司',
-          'longitude': 120.86742401123047,
-          'latitude': 32.00920867919922,
-          'type': 1
-        },
-        {
-          'name': '江苏德轩堂医药（集团）有限公司江苏省肿瘤医院药房',
-          'longitude': 118.79000091552735,
-          'latitude': 32.06999969482422,
-          'type': 1
-        },
-        {
-          'name': '连云港德益康连锁大药房',
-          'longitude': 119.16741943359375,
-          'latitude': 34.60169982910156,
-          'type': 1
-        },
-        {
-          'name': '广州德轩堂药房有限公司',
-          'longitude': 113.29737854003906,
-          'latitude': 23.12896156311035,
-          'type': 1
-        },
-        {
-          'name': '重庆德轩堂药房有限公司',
-          'longitude': 106.56463623046875,
-          'latitude': 29.520980834960939,
-          'type': 1
-        },
-        {
-          'name': '云南德轩堂药业有限公司',
-          'longitude': 102.69091796875,
-          'latitude': 25.04450225830078,
-          'type': 1
-        },
-        {
-          'name': '杭州德轩堂众康药房有限公司',
-          'longitude': 120.18572235107422,
-          'latitude': 30.260610580444337,
-          'type': 1
-        },
-        {
-          'name': '合肥德轩堂药房有限公司',
-          'longitude': 117.30792236328125,
-          'latitude': 31.793212890625,
-          'type': 1
-        },
-        {
-          'name': '石家庄宁益德轩堂药房有限公司',
-          'longitude': 114.48253631591797,
-          'latitude': 38.06251525878906,
-          'type': 1
-        },
-        {
-          'name': '福州德轩堂医药有限公司',
-          'longitude': 119.30177307128906,
-          'latitude': 26.07280731201172,
-          'type': 1
-        },
-        {
-          'name': '深圳德轩堂药房有限公司',
-          'longitude': 114.09757232666016,
-          'latitude': 22.561246871948243,
-          'type': 1
-        },
-        {
-          'name': '长沙德轩堂药房有限公司',
-          'longitude': 112.9884033203125,
-          'latitude': 28.216472625732423,
-          'type': 1
-        },
-        {
-          'name': '天津德轩堂药房有限公司',
-          'longitude': 117.17594909667969,
-          'latitude': 39.06995391845703,
-          'type': 1
-        },
-        {
-          'name': '济南德轩堂药房有限公司',
-          'longitude': 116.99533081054688,
-          'latitude': 36.65618133544922,
-          'type': 1
-        },
-        {
-          'name': '河南德轩堂药房有限公司',
-          'longitude': 113.70039367675781,
-          'latitude': 34.75701141357422,
-          'type': 1
-        },
-        {
-          'name': '成都德轩堂药房有限公司',
-          'longitude': 104.0665512084961,
-          'latitude': 30.628673553466798,
-          'type': 1
-        },
-        {
-          'name': '宿迁德轩堂药房有限公司',
-          'longitude': 118.29732513427735,
-          'latitude': 33.94375991821289,
-          'type': 1
-        },
-        {
-          'name': '江苏德轩堂医药（集团）有限公司南京市第二医院药房',
-          'longitude': 118.7688980102539,
-          'latitude': 32.08869934082031,
-          'type': 1
-        },
-        {
-          'name': '泰州德轩堂药房有限公司',
-          'longitude': 119.92945861816406,
-          'latitude': 32.47151565551758,
-          'type': 1
-        },
-        {
-          'name': '苏州延顺堂药房有限公司',
-          'longitude': 120.61083984375,
-          'latitude': 31.34779930114746,
-          'type': 2
-        }
-      ]
       // 输入经纬度和名称，标记
-      function bindPoint(longti, lati, name, type) {
+      function bindPoint(longti, lati, name, isDtp) {
         // map.clearOverlays();//清空原来的标注
 
         var pt = null
@@ -277,14 +123,14 @@ export default {
         // 添加点击事件
         // marker.addEventListener('click', function () { alert(name) })
         // 将标记放入标记数组
-        if (type === 1) {
+        if (isDtp) {
           markers1.push(marker)
         } else {
           markers2.push(marker)
         }
       }
-      pointList.forEach(item => {
-        bindPoint(item.longitude, item.latitude, item.name, item.type)
+      this.shopArr.forEach(item => {
+        bindPoint(item.longitude, item.latitude, item.name, item.isDtp)
       })
       // 调用API聚合函数将标记数组显示在地图上
       var markerClusterer = new BMapLib.MarkerClusterer(map, { markers: markers1 })
@@ -442,6 +288,11 @@ export default {
         map.addOverlay(ply)
         map.setViewport(ply.getPath()) // 调整视野
       })
+    }
+  },
+  watch: {
+    shopArr() {
+      this.initMap()
     }
   }
 }
